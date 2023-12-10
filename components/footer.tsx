1ae17ebb2c2c4fc0ef0d2f9ba8358734a3151796
Ozgur32 <odemirbacak4985@gmail.com>
2023-12-10 13:04:02 +0300

    final

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    const currentYear = new Date().getFullYear()
+
+    return (
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-xs text-black">&copy; {currentYear} OzgurDevFolio, Inc. All right reserved.</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
